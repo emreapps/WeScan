497b3be76e59129ec6f2dbbd827dcbfe6ddd0d99
emreapps <90357111+emreapps@users.noreply.github.com>
2021-09-26 22:32:03 +0300

    Update EditScanCornerView.swift

--- WeScan/Common/EditScanCornerView.swift
+++ WeScan/Common/EditScanCornerView.swift
@@ -53,7 +53,7 @@ final class EditScanCornerView: UIView {
     override func draw(_ rect: CGRect) {
         super.draw(rect)
         
-        circleLayer.lineWidth = CGFloat(isHighlighted ? 4.0 : 9) // Changed by Emre
+        circleLayer.lineWidth = CGFloat(isHighlighted ? 4.0 : 8.5) // Changed by Emre
         let insetDxDy = CGFloat(isHighlighted ? 2.0 : 4.0)         // Changed by Emre
         let bezierPath = UIBezierPath(ovalIn: rect.insetBy(dx: insetDxDy, dy: insetDxDy)) // Changed by Emre
         
